a40dda17e630e3a8f1ae437b34e9b2ae482441b1
TonyCross <starovoitov.anton@gmail.com>
2017-11-22 23:12:51 +0500

    Открытие мб файла и конвертирование в хтмл

--- site_maker.py
+++ site_maker.py
@@ -3,7 +3,10 @@ from livereload import Server
 
 
 def convert_md_to_html():
-    pass
+    with open('./articles/0_tutorial/7_codenvy.md', 'r', encoding='utf-8') as md_file_handler:
+        html = markdown.markdown(md_file_handler.read())
+        print(html)
+        # TODO сохранение в хтмл
 
 
 def make_site():
@@ -11,6 +14,7 @@ def make_site():
 
 
 if __name__ == '__main__':
+    convert_md_to_html()
     server = Server()
     server.watch('templates/*.html', make_site)
     # TODO watch for changes in markdown articles
