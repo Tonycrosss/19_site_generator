9c8265b77dce9f666d25e9ee3a7eb1102a49d61f
TonyCross <starovoitov.anton@gmail.com>
2017-11-22 23:31:01 +0500

    сохранение в хтмл

--- site_maker.py
+++ site_maker.py
@@ -5,8 +5,9 @@ from livereload import Server
 def convert_md_to_html():
     with open('./articles/0_tutorial/7_codenvy.md', 'r', encoding='utf-8') as md_file_handler:
         html = markdown.markdown(md_file_handler.read())
-        print(html)
-        # TODO сохранение в хтмл
+    with open('./html.html', 'w', encoding='utf-8') as html_file_handler:
+        html_file_handler.write(html)
+        # TODO сохранение всех мд в хтмл
 
 
 def make_site():
