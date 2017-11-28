c2962b9476c769dd60442805d04c09209e76659f
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-11-28 17:34:37 +0500

    Генерация страниц

--- site_maker.py
+++ site_maker.py
@@ -3,12 +3,13 @@ from livereload import Server
 import jinja2
 import json
 
+
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader('templates'))
 
 
-def convert_md_to_html():
-    with open('./articles/0_tutorial/7_codenvy.md', 'r', encoding='utf-8') as md_file_handler:
+def convert_md_to_html(filepath):
+    with open(str(filepath), 'r', encoding='utf-8') as md_file_handler:
         html = markdown.markdown(md_file_handler.read())
     # with open('./html.html', 'w', encoding='utf-8') as html_file_handler:
     #     html_file_handler.write(html)
@@ -16,14 +17,23 @@ def convert_md_to_html():
         return html
 
 
-def create_index_page(topics_data):
+def create_index_page(topics_data, articles_data):
     index_template = env.get_template('index.html')
     with open('./site/index.html', 'w', encoding='utf-8') as index_handler:
-        index_handler.write(index_template.render(topics_data=topics_data))
+        index_handler.write(index_template.render(topics_data=topics_data, articles_data=articles_data))
 
 
 def make_site(topics_data):
     create_index_page(topics_data)
+
+
+def create_static_pages(json_data):
+    page_template = env.get_template('page.html')
+    for article in json_data['articles']:
+        html = convert_md_to_html('./articles/{}'.format(article['source']))
+        html_file_name = './site/pages/{}.html'.format(article['title'])
+        with open(html_file_name, 'w', encoding='utf-8') as html_page_handler:
+            html_page_handler.write(page_template.render(html_content=html))
 
 
 def load_config():
@@ -34,8 +44,9 @@ def load_config():
 
 if __name__ == '__main__':
     json_data = load_config()
-    print(json_data['topics'])
-    create_index_page(topics_data=json_data['topics'])
+    print(json_data)
+    create_index_page(topics_data=json_data['topics'], articles_data=json_data['articles'])
+    create_static_pages(json_data)
     server = Server()
     # server.watch('templates/*.html', make_site(topics_data))
     # TODO watch for changes in markdown articles
