ca00ea063bca4b61f1f66bcd32a474477709ecd0
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-11-28 16:45:15 +0500

    Создается index.html используя шаблоны

--- site_maker.py
+++ site_maker.py
@@ -3,6 +3,9 @@ from livereload import Server
 import jinja2
 import json
 
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader('templates'))
+
 
 def convert_md_to_html():
     with open('./articles/0_tutorial/7_codenvy.md', 'r', encoding='utf-8') as md_file_handler:
@@ -13,25 +16,27 @@ def convert_md_to_html():
         return html
 
 
-def make_site():
-    print(1)
+def create_index_page(topics_data):
+    index_template = env.get_template('index.html')
+    with open('./site/index.html', 'w', encoding='utf-8') as index_handler:
+        index_handler.write(index_template.render(topics_data=topics_data))
+
+
+def make_site(topics_data):
+    create_index_page(topics_data)
+
 
 def load_config():
-    with open('./config.json', 'r') as file_handler:
+    with open('./config.json', 'r', encoding='utf-8') as file_handler:
         json_data = json.load(file_handler)
-    for topic in json_data['topics']:
-        print(topic)
-    return json
+    return json_data
 
 
 if __name__ == '__main__':
-    load_config()
-    env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader('templates'))
-    template = env.get_template('index.html')
-    rendered_template = template.render(html=convert_md_to_html())
-    print(rendered_template)
+    json_data = load_config()
+    print(json_data['topics'])
+    create_index_page(topics_data=json_data['topics'])
     server = Server()
-    server.watch('templates/*.html', make_site)
+    # server.watch('templates/*.html', make_site(topics_data))
     # TODO watch for changes in markdown articles
     server.serve(root='site/') # folder to serve html files from
