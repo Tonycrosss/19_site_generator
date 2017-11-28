c2c61023c032b561333f10e0ca74716d6d06fa2f
TonyCross <starovoitov.anton@gmail.com>
2017-11-28 09:46:09 +0000

    added html to template

--- site_maker.py
+++ site_maker.py
@@ -1,13 +1,15 @@
 import markdown
 from livereload import Server
+import jinja2
 
 
 def convert_md_to_html():
     with open('./articles/0_tutorial/7_codenvy.md', 'r', encoding='utf-8') as md_file_handler:
         html = markdown.markdown(md_file_handler.read())
-    with open('./html.html', 'w', encoding='utf-8') as html_file_handler:
-        html_file_handler.write(html)
+    # with open('./html.html', 'w', encoding='utf-8') as html_file_handler:
+    #     html_file_handler.write(html)
         # TODO сохранение всех мд в хтмл
+        return html
 
 
 def make_site():
@@ -15,7 +17,11 @@ def make_site():
 
 
 if __name__ == '__main__':
-    convert_md_to_html()
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader('templates'))
+    template = env.get_template('index.html')
+    rendered_template = template.render(html=convert_md_to_html())
+    print(rendered_template)
     server = Server()
     server.watch('templates/*.html', make_site)
     # TODO watch for changes in markdown articles
